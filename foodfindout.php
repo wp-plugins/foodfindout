2b89668ddc4025849def8b396ab77952576276a3
foodfindout <foodfindout@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-03 07:28:39 +0000

    fixed errors in main php file
    git-svn-id: https://plugins.svn.wordpress.org/foodfindout/trunk@1173368 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- foodfindout.php
+++ foodfindout.php
@@ -1,25 +1,24 @@
-
-/**
- *Plugin Name: foodfindout
- *Plugin URI: http://wordpress.org/plugins/foodfindout/
- *Description: gets restaurant info from foodfindout server
- *Author: Sunny
- *Version: 1.0.1
- */
-
-if(class_exists('Twig_Autoloader')==false){
-    require_once(__DIR__.'/Twig-1.18.0/lib/Twig/Autoloader.php');
-}
-Twig_Autoloader::register();
-
-function insert_rest_info($atts){
-    $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT']."/templates");
-    $twig = new Twig_Environment($loader,array('cache' => $_SERVER['DOCUMENT_ROOT']."/templates/cache",));
-    $a = shortcode_atts( array('name' => 'default','template'=>'default'), $atts );
-    $template = $twig->loadTemplate($a['template']);
-    $jsonarray=json_decode(file_get_contents("http://api.foodfindout.com/api/menu/".$a['name']));
-    echo $template->render(array ('restaurant' =>$jsonarray ));
-}
-add_shortcode( 'restaurantinfo', 'insert_rest_info' );
-
+<?php
+    /**
+     *Plugin Name: foodfindout
+     *Plugin URI: http://wordpress.org/plugins/foodfindout/
+     *Description: gets restaurant info from foodfindout server
+     *Author: Sunny
+     *Version: 1.0.1
+     */
+    if(class_exists('Twig_Autoloader')==false){
+        require_once(__DIR__.'/Twig-1.18.0/lib/Twig/Autoloader.php');
+    }
+    Twig_Autoloader::register();
+    
+    function insert_rest_info($atts){
+        $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT']."/templates");
+        $twig = new Twig_Environment($loader,array('cache' => $_SERVER['DOCUMENT_ROOT']."/templates/cache",));
+        $a = shortcode_atts( array('name' => 'default','template'=>'default'), $atts );
+        $template = $twig->loadTemplate($a['template']);
+        $jsonarray=json_decode(file_get_contents("http://api.foodfindout.com/api/menu/".$a['name']));
+        echo $template->render(array ('restaurant' =>$jsonarray ));
+    }
+    add_shortcode( 'restaurantinfo', 'insert_rest_info' );
+    
 ?>
