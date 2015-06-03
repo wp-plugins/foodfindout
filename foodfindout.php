38de5ad7f967047271b292f2e93b5ce97fe36727
foodfindout <foodfindout@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-03 07:00:50 +0000

    Template loaded from file name passed through shortcode
    git-svn-id: https://plugins.svn.wordpress.org/foodfindout/trunk@1173359 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- foodfindout.php
+++ foodfindout.php
@@ -1,25 +1,24 @@
-<?php
 
 /**
-*Plugin Name: foodfindout
-*Plugin URI: http://wordpress.org/plugins/foodfindout/
-*Description: gets restaurant info from foodfindout server
-*Author: Sunny
-*Version: 1.0.0
-*/
+ *Plugin Name: foodfindout
+ *Plugin URI: http://wordpress.org/plugins/foodfindout/
+ *Description: gets restaurant info from foodfindout server
+ *Author: Sunny
+ *Version: 1.0.0
+ */
 
 if(class_exists('Twig_Autoloader')==false){
-       require_once(__DIR__.'/Twig-1.18.0/lib/Twig/Autoloader.php');
+    require_once(__DIR__.'/Twig-1.18.0/lib/Twig/Autoloader.php');
 }
 Twig_Autoloader::register();
 
-function insert_rest_info($atts){      
-       $loader = new Twig_Loader_Filesystem(__DIR__);
-       $twig = new Twig_Environment($loader);
-       $template = $twig->loadTemplate('template.html');	
-       $a = shortcode_atts( array('name' => 'default'), $atts );
-       $jsonarray=json_decode(file_get_contents("http://api.foodfindout.com/api/menu/".$a['name']));
-       echo $template->render(array ('restaurant' =>$jsonarray     ));  
+function insert_rest_info($atts){
+    $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT']."/templates");
+    $twig = new Twig_Environment($loader,array('cache' => $_SERVER['DOCUMENT_ROOT']."/templates/cache",));
+    $a = shortcode_atts( array('name' => 'default','template'=>'default'), $atts );
+    $template = $twig->loadTemplate($a['template']);
+    $jsonarray=json_decode(file_get_contents("http://api.foodfindout.com/api/menu/".$a['name']));
+    echo $template->render(array ('restaurant' =>$jsonarray ));
 }
 add_shortcode( 'restaurantinfo', 'insert_rest_info' );
 
